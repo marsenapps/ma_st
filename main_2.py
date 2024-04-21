82b1bf0069e8046099cd9e9d45a2a028ca33a6a3
marsenapps <144794792+marsenapps@users.noreply.github.com>
2024-04-21 19:47:15 +0200

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -5,7 +5,12 @@ import pandas as pd
 import streamlit.components.v1 as components
 
 components.html("<html><body><div style=\"background-color:tomato;padding:10px\"><h1 style=\"color:white;text-align:center;\"> Welcome to MSTECH</h1></div></body></html>")
+
+
 st.title("Happy to learn with you all...")
+
+components.html("<html><body><div style=\"background-color:tomato; padding:10px\"><h2 style=\"color:white;text-align:center;\"> Welcome to the Portal...</h2><div></div></body></html>")
+
 
 html_temp="""
 <div style="background-color:tomato; padding:10px">
