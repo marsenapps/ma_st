d4998d2ff837626013bf970a6284a1b8d576e3eb
marsenapps <144794792+marsenapps@users.noreply.github.com>
2024-04-21 18:55:40 +0200

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -1,4 +1,3 @@
-### https://www.youtube.com/watch?v=Ll-PBGeMZ6I&list=PLjyA8VTW-RcVI7deAqL0OXuIIptjMwlVL&index=2
 
 import streamlit as st
 import pandas as pd
@@ -6,24 +5,24 @@ import pandas as pd
 import streamlit.components.v1 as components
 
 components.html("<html><body><div style=\"background-color:tomato;padding:10px\"><h1 style=\"color:white;text-align:center;\"> Welcome to MSTECH</h1></div></body></html>")
-
+#st.title("Happy to learn with you all...")
 
 html_temp="""
 <div style="background-color:tomato; padding:10px">
-<h2 style="color:white;text-align:center;">Very Excited to learn streamlit...</h2
+<h2 style="color:white;text-align:center;"> Welcome to Streamlit</h2
 <div>
 """
-st.write("Forward step...")
+st.write("Here you can lean everything about Streamlit")
 
 ## Part#2: Creating dataset and viz
 df=pd.DataFrame({'First':['None',1,2,3,4,5],
 	'Second':[6,7,8,9,10,11],
 	'Third':[12,13,14,15,16,17]})
-#>st.sidebar.write(df)
+#st.sidebar.write(df)
 ##or
 #>df ## magic command
 ## Visualization###
-#>st.line_chart(df)
+#st.line_chart(df)
 
 ### sidebar###
 st.sidebar.write(df)
@@ -41,16 +40,7 @@ if opt=='None':
 else:
 	st.write("You just selected: ",opt)
 
-### Streamlit Tutorial For beginners - #Part2 Slider, ProgressBar, Beta Columns, Beta Expander
-##############################################################################################
 
-
-	## 2. Progress Bar
-	import time
-	pro=st.progress(0)
-	for i in range(50):
-		pro.progress(i+1)
-		time.sleep(0.1)
 
 	## 4. Beta Expander
 #expander=st.beta_expander("Hello")
@@ -60,4 +50,18 @@ expander.write("How are you?")
 
 ### Streamlit Tutorial for Beginners - #part3 Spinner Widget, types of messages & Component widget
 ###################################################################################################
+
+##1. Spinner
+
+with st.spinner("Loading..."):
+	time.sleep(5)
+
+	##2. Types of messages
+
+st.success("Loading Skills...")#Successful
+
+
+ ## 3. Balloons animations
+with st.balloons():
+	time.sleep(10)
 
